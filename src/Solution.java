0ce069ccf4d053a8c98c8cc26f96100b8ad699e6
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 23:11:05 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,41 @@
+/* 
+Лошадь и Пегас
+Лошадь и Пегас
+Отредактировать два класса: Horse (лошадь) и Pegas (пегас).
+Унаследовать пегаса от лошади.
+
+
+Требования:
+1. В классе Solution должен быть класс Horse (лошадь).
+2. Класс Horse должен быть public.
+3. В классе Solution должен быть класс Pegas (пегас).
+4. Класс Pegas должен быть public.
+5. Класс Pegas должен быть унаследован от класса Horse.
+
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegas {
+
+    }
+}
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegas extends Horse{
+
+    }
+}
